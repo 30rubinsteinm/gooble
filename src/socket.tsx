3eeba37aa619ca207ad53e683fc88f3d26a10a14
SupKittyMeow <77246128+SupKittyMeow@users.noreply.github.com>
2025-12-02 15:43:02 -0800

    Update socket.tsx

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 const URL = import.meta.env.PROD
-  ? "https://goobapp-backend.onrender.com"
+  ? "https://goobapp-server.koyeb.app/"
   : "http://localhost:3000"; // If import.meta.env.PROD is true, then you are in production. Otherwise just use localhost
 
 export const socket = io(URL, {
