69dc2be7ae6d3b9a8012988b50e59126b942037d
mrubinstein2030 <217809840+mrubinstein2030@users.noreply.github.com>
2025-12-05 18:53:59 +0000

    some stuff

--- src/components/supabase/Client.tsx
+++ src/components/supabase/Client.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wfdcqaqihwsilzegcknq.supabase.co";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_KEY as string;
 
 export const Client = supabasePublishableKey ? createClient(supabaseUrl, supabasePublishableKey) : null;
--- src/socket.tsx
+++ src/socket.tsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 const URL = import.meta.env.PROD
-  ? "https://goobapp-server.koyeb.app/"
+  ? import.meta.env.VITE_BACKEND_URL
   : "http://localhost:3000"; // If import.meta.env.PROD is true, then you are in production. Otherwise just use localhost
 
 export const socket = io(URL, {
