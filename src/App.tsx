bccace1cf326ce020f64bd12db604ad0165469af
SupKittyMeow <supkittymeow@gmail.com>
2025-12-13 02:07:38 -0800

    test thing yes

--- src/App.tsx
+++ src/App.tsx
@@ -169,7 +169,6 @@ const App = () => {
   }, []);
 
   const addNewInput = (newMessage: ChatMessageObject) => {
-    console.log(newMessage);
     newMessage.messageTime = new Date(newMessage.messageTime); // Websockets can't accept Dates, so they turn them into strings. This turns it back
     setMessages((prevMessage) =>
       prevMessage.length < 200
@@ -226,7 +225,7 @@ const App = () => {
     });
 
     setProfile(newProfile);
-    socket.emit("add to active users list", newProfile);
+    // socket.emit("add to active users list", newProfile);
   };
 
   const retrieveRecentMessages = () => {
@@ -254,10 +253,11 @@ const App = () => {
             retrieveUserData(session);
             retrieveRecentMessages();
             retrieveActiveUsers();
+            socket.emit("add to active users list", profile);
           } else if (_event == "TOKEN_REFRESHED") {
             retrieveUserData(session);
             retrieveActiveUsers();
-            socket.emit("add to active users list", profile); // bc it disconnects you need to readd to active list
+            socket.emit("add to active users list", profile);
           }
         } else {
           setSession(null);
